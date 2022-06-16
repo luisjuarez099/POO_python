b9cbb7b70db1ae168c5f569e612d94c617fa4285
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-06-16 17:57:03 -0500

    Nueva funcion de envio

--- class_attribute/sell_phone.py
+++ class_attribute/sell_phone.py
@@ -4,16 +4,17 @@ paradigma orientado a objetos
 class Item:
     #---------------------------Atributos de clase
     impuesto=0.16
+    envio=30
     #---------------------------Atributos o caracteristicas
     def __init__(self,precio:float,modelo:str,marca:str):
         #Atributos de Inatancia
         self.modelo=modelo
         self.marca=marca
         self.precio=precio
-    #-----------------------------Metodos. ACCIONES para calcular. 
+    #-----------------------------Metodos. ACCIONES para calcular. Metodos de Instancia.
     def Pago_total(self):
         #Pago total a pagar precio por cantidad. (Precio en Dollar)
-        self.cantidad=int(input(f"Digta cantidad de Telefonos {telefono1.marca} que quiere: "))
+        self.cantidad=int(input(f"Digta cantidad de Telefonos que quiere: "))
         self.total=self.precio*self.cantidad
         return self.total
 
@@ -22,22 +23,20 @@ class Item:
         self.impuesto=self.total * Item.impuesto 
         self.precio_mas_impuesto=self.impuesto + self.total 
         return self.precio_mas_impuesto
-    def envio_internacional(self,envio):
-        '''***TERMINAR MI ENVIO CON PARAMETRO DENTRO DE ESTA FUNCIOS
-        AVERIGUAR QUE ES CADA COSA ESCRITA DENTRO DE ESTE PROGRAMA'''
-        pass
-    
+    def envio_internacional(self):
+        return self.precio_mas_impuesto + Item.envio
+#Instancia 2   
 telefono1=Item(100,"AXP123","Xiomi")
 print(f"Modelo es {telefono1.modelo}")
 print(f"Marca del telefono: {telefono1.marca}")
 print(f"Pago total del telefono: {telefono1.Pago_total()}")
 print(f"Pago de Impuestos del producto: {telefono1.impuestos()} pesos")
-
+print(f"El precio de envio es de : {telefono1.envio_internacional()}")
 print()
-
+#Instancia 1
 telefono2=Item(500,"13","Apple")
 print(f"Modelo es {telefono2.modelo}")
 print(f"Marca del telefono: {telefono2.marca}")
 print(f"Pago total del telefono: {telefono2.Pago_total()}")
 print(f"Pago de Impuestos del producto: {telefono2.impuestos()} pesos")
-
+print(f"El precio de envio es de : {telefono2.envio_internacional()}")
