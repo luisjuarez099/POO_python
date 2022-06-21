7786fd4ccc905d61ac6e8ac71deef781f50eec73
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-06-21 11:11:40 -0500

    Hicimos herencia del cliente <- sueper a vendedor->sub

--- Herencia/rentacar.py
+++ Herencia/rentacar.py
@@ -1,4 +1,25 @@
 
-class cliente:
-    def __init__(self,nombre,edad,) -> None:
-        pass
+class Cliente:
+    def __init__(self,nombre,ID) -> None:
+        self.nombre=nombre
+        self.ID=ID
+    def saludo(self):
+        print(f"Nombre del cliente: {self.nombre}, ID del Cliente : {self.ID}")
+
+
+class Vendedor(Cliente):
+    def __init__(self, nombre, IDV) -> None:
+        super().__init__(nombre, IDV)
+        self.IDV=IDV
+    def atiende(self):
+        print(f"Vendedor: {self.nombre}, ID del Vendedor: {self.IDV}")
+
+cliente=Cliente("Luis","@CLJP9055")
+vendedor=Vendedor("Emilio","$VEK5571")
+
+
+
+if __name__=="__main__":
+    cliente.saludo()
+    vendedor.atiende()
+
