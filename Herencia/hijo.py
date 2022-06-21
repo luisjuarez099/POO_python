c9b0566613b7384fd29b6347c209b8fc992a28a5
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-06-20 21:23:55 -0500

    Herencia estudiantes y trabajador

---
+++ Herencia/hijo.py
@@ -0,0 +1,41 @@
+class Persona:
+    def __init__(self, nombre, edad) -> None:
+        self.nombre=nombre
+        self.edad=edad
+        
+    def Presentarse(self):
+        print(f"Hola, me llamo {self.nombre}, mi edad es de {self.edad} anios y en la universidad {self.escuela}  estudio la carrera de {self.carrera} con un promedio de {self.promedio}. Mucho gusto")
+
+
+# persona=Persona("emilio",23,"UNEDL",80)
+# persona.Presentarse()
+
+class Alumno(Persona):#Hereda de la clase Persona
+    #propio constructor
+    def __init__(self, nombre, edad, escuela,promedio,carrera) -> None:
+        super().__init__(nombre, edad)
+        self.promedio=promedio
+        self.carrera=carrera
+        self.escuela=escuela
+
+    def SigSemestre(self):
+        if self.promedio >= 70 and self.promedio <=100:
+            print("Pasas al siguiente semestre")
+        else:
+            print("Estas reprobado.")
+
+class Trabajador(Persona):
+    def __init__(self, nombre,edad, sueldo, puesto) -> None:
+        super().__init__(nombre, edad)
+        self.sueldo=sueldo
+        self.puesto=puesto
+    def calcularSueldo(self):
+        return  self.sueldo / 6
+
+alumno=Alumno("Luis",23,"UNEDL",65,"Ing. Software")
+alumno.Presentarse()
+alumno.SigSemestre()
+
+trabajador=Trabajador("Julio",55,1200,"programador")
+print(f"Hola, me llamo {trabajador.nombre}, mi edad es de {trabajador.edad} anios y trabajo como {trabajador.puesto} Mucho gusto")
+print(f"Ganas por dia : {trabajador.calcularSueldo()} pesos")
