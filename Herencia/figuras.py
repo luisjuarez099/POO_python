722d01eb3ea6b2e367261d496944f26495c64554
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-07-10 22:24:14 -0500

    Figuras Geometricas

--- Herencia/figuras.py
+++ Herencia/figuras.py
@@ -15,9 +15,18 @@ class Rectangulo(Triangulo):
     def perimetro(self):
         return 2*(self.base+self.altura)
 
+class Octagono(Rectangulo):
+    def __init__(self, apotema) -> None:
+        super().__init__(apotema,apotema)
+
+
+'''El Perimetro del octagono es incorrecto, simplemente estoy jugando con la Herencia'''
 if __name__=='__main__':
     triangulo=Triangulo(base=2,altura=4)
     print(triangulo.area())
 
     cuadrado=Rectangulo(4,6)
     print(cuadrado.perimetro())
+
+    octagono=Octagono(5)
+    print(octagono.perimetro())
